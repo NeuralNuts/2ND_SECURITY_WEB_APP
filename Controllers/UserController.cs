e4c9ccb3664c0b8cfeb597f2187f7ba74a5fb30d
NeuralNuts <wilsonzakk8@gmail.com>
2023-05-15 14:15:26 +1000

    hash

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using _2ND_SECURITY_WEB_APP.Models;
 using _2ND_SECURITY_WEB_APP.Repository;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System;
 using System.Text.RegularExpressions;
 
 namespace _2ND_SECURITY_WEB_APP.Controllers
@@ -17,6 +19,17 @@ namespace _2ND_SECURITY_WEB_APP.Controllers
             _userRepository = user_repository;
         }
         #endregion
+
+        public static string HashPassword(string password)
+        {
+            var salt = BCrypt.Net.BCrypt.GenerateSalt();
+            return BCrypt.Net.BCrypt.HashPassword(password, salt);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
 
         #region Email validation using regex
         static bool EmailValidation(string email)
@@ -51,25 +64,72 @@ namespace _2ND_SECURITY_WEB_APP.Controllers
             string message;
             var check_status = _userRepository.GetUsers().Result.Where(m => m.email == user_model.email).FirstOrDefault();
             var email_valadtion = EmailValidation(user_model.email);
+            var hash_password = HashPassword(user_model.password);
+
+            //byte[] data = Encoding.ASCII.GetBytes(user_model.password);
+            //data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+            //String hash = Encoding.ASCII.GetString(data);
 
             try
             {
                 if (check_status != null)
                 {
-                    message = "USER DETAILS INVALID";
+                    message = "Email allready in use";
                 }
 
                 else if (email_valadtion == false)
                 {
-                    message = "ENTER AN EMAIL";
+                    message = "Enter an email";
                 }
 
                 else
                 {
-                    message = "USER DETAILS VALID";
+                    message = "Account created";
+                    user_model.hashPassword = hash_password.ToString();
                     user_model.GUID = Guid.NewGuid().ToString();
                     await _userRepository.PostUser(user_model);
                 }
+                return Json(message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Authenticates user login
+        [HttpGet]
+        [Route("AuthenticateLogin")]
+
+        public async Task<IActionResult> AuthenticateLogin(UserModel? user_model)
+        {
+            string message;
+            var login_status = _userRepository.GetUsers().Result.Where(m => m.email == user_model.email && m.password == user_model.password).FirstOrDefault();
+            var hash_password = _userRepository.GetUsers().Result.Where(u => u.hashPassword);
+            var email_valadtion = EmailValidation(user_model.email);
+
+            var hash_auth = VerifyPassword(user_model.password, user_model.hashPassword); 
+
+            try
+            {
+                if (login_status != null)
+                {
+                    //string id = HttpContext.Session.Id;
+                    //HttpContext.Session.GetString(id);
+                    message = "LOGIN VALID";
+                }
+
+                if (hash_auth == true)
+                {
+                    message = "Hash Bad";
+                }
+
+                else
+                {
+                    message = "LOGIN INVALID";
+                }
+
                 return Json(message);
             }
             catch (Exception ex)
--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using _2ND_SECURITY_WEB_APP.Context;
 using _2ND_SECURITY_WEB_APP.Models;
 using Dapper;
+using System.Text;
 
 namespace _2ND_SECURITY_WEB_APP.Repository
 {
@@ -26,11 +27,25 @@ namespace _2ND_SECURITY_WEB_APP.Repository
         }
         #endregion
 
+        #region Gets user_id and updates details
+        public async Task<IEnumerable<UserModel>> CheckUsersId()
+        {
+            var query = "SELECT user_id FROM [User]";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var users = await connection.QueryAsync<UserModel>(query);
+
+                return users.ToList();
+            }
+        }
+        #endregion
+
         #region Creates new user account
         public async Task PostUser(UserModel userModel)
         {
-            var query = "INSERT INTO [User] (email, password, role, GUID) " +
-                        "VALUES (@email, @password, @role, @GUID) ";
+            var query = "INSERT INTO [User] (email, password, hashPassword, role, GUID) " +
+                        "VALUES (@email, @password, @hashPassword, @role, @GUID) ";
 
             using (var connection = _context.CreateConnection())
             {
